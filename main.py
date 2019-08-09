99581c204635aaea791d58d5b350c05f4b44984d
taekyulee <taekyulee@yonsei.ac.kr>
2019-08-09 15:58:08 +0900

    * controlled by system arguments

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-from model import *
+import sys
+
 from data import *
+from model import *
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
+if __name__ == '__main__':
+    data_set_name = sys.argv[1]
+    test_result_folder = sys.argv[2]
 
-data_gen_args = dict(rotation_range=0.2,
-                    width_shift_range=0.05,
-                    height_shift_range=0.05,
-                    shear_range=0.05,
-                    zoom_range=0.05,
-                    horizontal_flip=True,
-                    fill_mode='nearest')
-myGene = trainGenerator(2,'data/membrane/train','image','label',data_gen_args,save_to_dir = None)
+    data_gen_args = dict(rotation_range=0.2,
+                         width_shift_range=0.05,
+                         height_shift_range=0.05,
+                         shear_range=0.05,
+                         zoom_range=0.05,
+                         horizontal_flip=True,
+                         fill_mode='nearest')
+    myGene = trainGenerator(2, "data/" + data_set_name + "/train", 'image', 'label', data_gen_args, save_to_dir=None)
 
-model = unet()
-model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
-model.fit_generator(myGene,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
+    model = unet()
+    model_checkpoint = ModelCheckpoint(data_set_name + ".hdf5", monitor='loss', verbose=1, save_best_only=True)
+    model.fit_generator(myGene, steps_per_epoch=300, epochs=1, callbacks=[model_checkpoint])
 
-testGene = testGenerator("data/membrane/test")
-results = model.predict_generator(testGene,30,verbose=1)
-saveResult("data/membrane/test",results)
+    testGene = testGenerator("data/" + data_set_name + "/test")
+    results = model.predict_generator(testGene, 30, verbose=1)
+
+    if not os.path.exists(test_result_folder):
+        os.makedirs(test_result_folder)
+    saveResult(test_result_folder, results)
