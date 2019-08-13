d0b05db3fbcf2d969ac6bdf9b98b8bbb3768234d
taekyulee <taekyulee@yonsei.ac.kr>
2019-08-13 17:20:18 +0900

    + add Test each epoch

--- training.py
+++ training.py
@@ -1,10 +1,9 @@
 import os
-import shutil
 import sys
 
 from keras.callbacks import ModelCheckpoint
 
-from data import train_generator, test_generator, save_result
+from data import train_generator
 from model import unet
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
@@ -23,17 +22,26 @@ if __name__ == '__main__':
     my_generator = train_generator(2, "data/" + data_set_name + "/train", 'image', 'label', data_gen_args,
                                    save_to_dir=None)
 
+    print("--- Training ---")
     model = unet()
     model_checkpoint = ModelCheckpoint(data_set_name + ".hdf5", monitor='loss', verbose=1, save_best_only=True)
     model.fit_generator(my_generator, steps_per_epoch=300, epochs=1, callbacks=[model_checkpoint])
 
-    testGene = test_generator("data/" + data_set_name + "/test")
-    results = model.predict_generator(testGene, 30, verbose=1)
+    print("--- Validation ---")
 
-    if not os.path.exists(test_result_folder):
-        os.makedirs(test_result_folder)
-    save_result(test_result_folder, results)
+    print("--- Test ---")
+    my_test_generator = train_generator(2, "data/" + data_set_name + "/test2", 'image', 'label', data_gen_args,
+                                        save_to_dir=None)
+    scores = model.evaluate_generator(my_test_generator, )
+    for index, metrics_name in enumerate(model.metrics_names):
+        print("%s: %.2f%%" % (model.metrics_names[index], scores[index] * 100))
 
-    files = os.listdir("data/" + data_set_name + "/test")
-    for file in files:
-        shutil.copy("data/" + data_set_name + "/test/" + file, test_result_folder)
+    # print("--- Predict ---")
+    # test_generator = test_via_path_generator("data/" + data_set_name + "/test")
+    # results = model.predict_generator(test_generator, 30, verbose=1)
+    # if not os.path.exists(test_result_folder):
+    #     os.makedirs(test_result_folder)
+    # save_result(test_result_folder, results)
+    # files = os.listdir("data/" + data_set_name + "/test")
+    # for file in files:
+    #     shutil.copy("data/" + data_set_name + "/test/" + file, test_result_folder)
